3bc732dce913282d2820eea56991c5a14db50ed2
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2024-08-15 07:58:53 +0530

    adde first

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -2,19 +2,31 @@
 // import the Calcclulator class from com.i27academy.build package
 import com.i27academy.builds.Calculator
 
-// An instance of the class called calculator is created
-Calculator calculator = new Calculator(this)
+def call(Map pipelineparams){
+    // An instance of the class called calculator is created
+    Calculator calculator = new Calculator(this)
 
-Pipeline {
-    agent any 
-    stages {
-        stage('AdditionStage') {
-            steps {
-                script {
-                   echo "Printing Sum of 2 numbers"
-                   println calculator.add(3,4) // add expects 2 parameters
+    Pipeline {
+        agent any 
+        environment {
+            APP_NAME = ${pipelineparams.appName}// this value should be coming from microservices 
+        }
+        stages {
+            stage('AdditionStage') {
+                steps {
+                    script {
+                    echo "Printing Sum of 2 numbers"
+                    println calculator.add(3,4) // add expects 2 parameters
+                    echo "****** Microservice Name is: ${APP_NAME} ********"
+                    }
+                }
+            }
+            stage ('SecondStage') {
+                steps {
+                    echo "Printing second stage"
                 }
             }
         }
     }
 }
+
